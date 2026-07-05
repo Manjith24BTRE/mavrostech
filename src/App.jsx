2adedfeb3e74ca6c6d62f3a00eb2694a5b21b8dc
Rudrik <rudrik28novjoshi@gmail.com>
2026-07-05 23:57:58 +0530

    new_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect, lazy, Suspense } from 'react'
+import { useEffect } from 'react'
+import Home from './pages/Home'
+import Team from './pages/Team'
+import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
-const Home = lazy(() => import('./pages/Home'))
-const Team = lazy(() => import('./pages/Team'))
-const Projects = lazy(() => import('./pages/Projects'))
 
 function App() {
   const location = useLocation();
@@ -42,13 +41,11 @@ function App() {
   return (
     <>
       <Header />
-      <Suspense fallback={null}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Team />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Team />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
       <Footer />
     </>
   )
